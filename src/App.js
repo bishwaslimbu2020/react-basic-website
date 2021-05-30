bbf563e71a9aa75023903cdf5be4fb34358dda54
bishwas <orange_fruits10@yahoo.com>
2021-05-30 11:01:43 +0100

    react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import { Switch, Route, Redirect } from 'react-router';
 import Home from "./component/Home";
+import About from "./component/About";
+import Service from "./component/Service";
+import Contact from "./component/Contact";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import Navbar from "./component/Navbar";
 
 function App() {
   return (
     <div>
-      <Home/>
+    <Navbar/>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/about" component={About}/>
+        <Route exact path="/service" component={Service}/>
+        <Route exact path="/contact" component={Contact}/>
+        <Redirect to= "/"/>
+      </Switch>
     </div>
   )
 }
