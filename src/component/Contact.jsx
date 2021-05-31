80bda2579df13293ad37893341c1e2421c89f22c
bishwas <orange_fruits10@yahoo.com>
2021-05-31 20:13:07 +0100

    service left

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -1,22 +1,63 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function Contact() {
+
+const [data, setData] = useState({
+    fullname:"",
+    email:"",
+    message:""
+});
+
+const inputEvent=(event)=>{
+    const{name, value}=event.target;
+    setData((preVal)=>{
+        return{
+            ...preVal,
+            [name]: value
+        }
+    })
+}
+
+const formSubmit=(e)=>{
+    e.preventDefault();
+    alert(`My name is ${data.fullname}. My email is ${data.email}. message: ${data.message}`)
+}
+
     return (
         <div>
             <div className="my-5">
                 <h1 className="text-center">Contact Us</h1>
             </div>
-            <div className="conatainer">
+            <div className="container">
                 <div className="row">
                     <div className="col-10 col-md-6 mx-auto">
-                        <div class="mb-3">
-                            <label for="exampleFormControlInput1" class="form-label">Email address</label>
-                            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
-                        </div>
-                        <div class="mb-3">
-                            <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                        </div>
+                        <form onSubmit={formSubmit}>
+                            <div className="mb-3">
+                                <label for="exampleFormControlInput1" className="form-label">Name</label>
+                                <input type="text"  className="form-control" id="exampleFormControlInput1" placeholder="bishwas limbu"
+                                name="fullname" 
+                                value={data.fullname} 
+                                onChange={inputEvent} />
+                            </div>
+                            <div className="mb-3">
+                                <label for="exampleFormControlInput1" className="form-label">Email address</label>
+                                <input type="email" className="form-control" id="exampleFormControlInput2"  placeholder="name@example.com"
+                                name="email" 
+                                value={data.email} 
+                                onChange={inputEvent} />
+                            </div>
+                            <div className="mb-3">
+                                <label for="exampleFormControlTextarea1" className="form-label">Message</label>
+                                <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="write here"
+                                name="message" 
+                                value={data.message} 
+                                onChange={inputEvent} >           
+                                </textarea>
+                            </div>
+                            <div className="col-12">
+                                <button className="btn btn-primary" type="submit">Submit form</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
